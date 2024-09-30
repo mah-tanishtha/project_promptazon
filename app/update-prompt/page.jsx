efd330b1e3d85fb8f0907ff8156ec0d7413bb345
unknown <mahajantani2121@gmail.com>
2024-09-30 23:16:35 +0530

    deployment-changes

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState,Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+
 import Form from "@components/Form";
 
 const UpdatePrompt = () => {
@@ -9,27 +10,18 @@ const UpdatePrompt = () => {
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
 
-  const [post, setPost] = useState({ prompt: "", tag: "" });
+  const [post, setPost] = useState({ prompt: "", tag: "", });
   const [submitting, setIsSubmitting] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const getPromptDetails = async () => {
-      try {
-        const response = await fetch(`/api/prompt/${promptId}`);
-        if (!response.ok) throw new Error("Failed to fetch prompt details");
-        const data = await response.json();
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
 
-        setPost({
-          prompt: data.prompt,
-          tag: data.tag,
-        });
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
     };
 
     if (promptId) getPromptDetails();
@@ -48,15 +40,10 @@ const UpdatePrompt = () => {
           prompt: post.prompt,
           tag: post.tag,
         }),
-        headers: {
-          "Content-Type": "application/json",
-        },
       });
 
       if (response.ok) {
         router.push("/");
-      } else {
-        throw new Error("Failed to update prompt");
       }
     } catch (error) {
       console.log(error);
@@ -65,17 +52,18 @@ const UpdatePrompt = () => {
     }
   };
 
-  if (loading) return <div>Loading prompt details...</div>;
-  if (error) return <div>Error: {error}</div>;
-
   return (
-    <Form
+    <Suspense>
+       <Form
       type='Edit'
       post={post}
       setPost={setPost}
       submitting={submitting}
       handleSubmit={updatePrompt}
     />
+
+    </Suspense>
+   
   );
 };
 
